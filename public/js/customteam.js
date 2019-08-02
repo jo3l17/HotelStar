e31a30614537466d6ed2fdcae37f8f63a7163dad
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2019-08-02 18:28:50 -0500

    avance 2 agosto

--- public/js/customteam.js
+++ public/js/customteam.js
@@ -1,5 +1,35 @@
 (function ($) {
     var index = $("#owl-index")
+    var modal = $("#owl-modal")
+
+    modal.owlCarousel({
+        items : 1,
+		itemsCustom : false,
+		itemsDesktop : [1199,1],
+		itemsDesktopSmall : [991,1],
+		itemsTablet: [768,1],
+		itemsTabletSmall: [600,1],
+		itemsMobile : [479,1],
+		singleItem : false,
+        itemsScaleUp : false,
+        margin:30,
+	 
+		//Autoplay
+		autoPlay : true,
+		stopOnHover : true,
+	 
+		// Navigation
+	 
+		//Pagination
+		pagination : true,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,
+
+    })
 
     index.owlCarousel({
 		items : 3,
@@ -30,8 +60,13 @@
 		// Responsive 
 		responsive: true,
 		responsiveRefreshRate : 200,
-		responsiveBaseWidth: window,    
+		responsiveBaseWidth: window,
 	});
 
+    
 
 })(jQuery)
+
+$(window).on('load',function(){
+    $('#myModal').modal('show');
+});
