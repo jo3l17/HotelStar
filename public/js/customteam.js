d5875cfeaf1693f31307729060ab77f517f9d627
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2019-08-09 10:41:40 -0500

    motor de reserva

--- public/js/customteam.js
+++ public/js/customteam.js
@@ -110,72 +110,72 @@
 	});
 
 	programas.owlCarousel({
-		items : 1,
-		itemsCustom : false,
-		itemsDesktop : [1199,1],
-		itemsDesktopSmall : [991,1],
-		itemsTablet: [768,1],
-		itemsTabletSmall: [600,1],
-		itemsMobile : [479,1],
-		singleItem : false,
-		itemsScaleUp : false,
-		margin:30,
+		items: 1,
+		itemsCustom: false,
+		itemsDesktop: [1199, 1],
+		itemsDesktopSmall: [991, 1],
+		itemsTablet: [768, 1],
+		itemsTabletSmall: [600, 1],
+		itemsMobile: [479, 1],
+		singleItem: false,
+		itemsScaleUp: false,
+		margin: 30,
 		//Autoplay
-		autoPlay : true,
-		stopOnHover : true,
+		autoPlay: true,
+		stopOnHover: true,
 		// Navigation
 		//Pagination
-		pagination : true,
+		pagination: true,
 		paginationNumbers: false,
 		// Responsive 
 		responsive: true,
-		responsiveRefreshRate : 200,
+		responsiveRefreshRate: 200,
 		responsiveBaseWidth: window,
 	})
 	lugares.owlCarousel({
-		items : 1,
-		itemsCustom : false,
-		itemsDesktop : [1199,1],
-		itemsDesktopSmall : [991,1],
-		itemsTablet: [768,1],
-		itemsTabletSmall: [600,1],
-		itemsMobile : [479,1],
-		singleItem : false,
-		itemsScaleUp : false,
-		margin:30,
+		items: 1,
+		itemsCustom: false,
+		itemsDesktop: [1199, 1],
+		itemsDesktopSmall: [991, 1],
+		itemsTablet: [768, 1],
+		itemsTabletSmall: [600, 1],
+		itemsMobile: [479, 1],
+		singleItem: false,
+		itemsScaleUp: false,
+		margin: 30,
 		//Autoplay
-		autoPlay : true,
-		stopOnHover : true,
+		autoPlay: true,
+		stopOnHover: true,
 		// Navigation
 		//Pagination
-		pagination : true,
+		pagination: true,
 		paginationNumbers: false,
 		// Responsive 
 		responsive: true,
-		responsiveRefreshRate : 200,
+		responsiveRefreshRate: 200,
 		responsiveBaseWidth: window,
 	})
 	establecimientos.owlCarousel({
-		items : 1,
-		itemsCustom : false,
-		itemsDesktop : [1199,1],
-		itemsDesktopSmall : [991,1],
-		itemsTablet: [768,1],
-		itemsTabletSmall: [600,1],
-		itemsMobile : [479,1],
-		singleItem : false,
-		itemsScaleUp : false,
-		margin:30,
+		items: 1,
+		itemsCustom: false,
+		itemsDesktop: [1199, 1],
+		itemsDesktopSmall: [991, 1],
+		itemsTablet: [768, 1],
+		itemsTabletSmall: [600, 1],
+		itemsMobile: [479, 1],
+		singleItem: false,
+		itemsScaleUp: false,
+		margin: 30,
 		//Autoplay
-		autoPlay : true,
-		stopOnHover : true,
+		autoPlay: true,
+		stopOnHover: true,
 		// Navigation
 		//Pagination
-		pagination : true,
+		pagination: true,
 		paginationNumbers: false,
 		// Responsive 
 		responsive: true,
-		responsiveRefreshRate : 200,
+		responsiveRefreshRate: 200,
 		responsiveBaseWidth: window,
 	})
 
@@ -185,3 +185,84 @@ $(window).on('load', function () {
 	$('#myModal').modal('show');
 
 });
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+var checkin = $('#fecha_inicio').datepicker({
+	format: 'dd-mm-yyyy',
+	onRender: function (date) {
+		return date.valueOf() < now.valueOf() ? 'disabled' : '';
+	}
+}).on('changeDate', function (ev) {
+	if (ev.date.valueOf() > checkout.date.valueOf()) {
+		var newDate = new Date(ev.date)
+		newDate.setDate(newDate.getDate() + 1);
+		checkout.setValue(newDate);
+	}
+	checkin.hide();
+	$('#fecha_termino')[0].focus();
+}).data('datepicker');
+var checkout = $('#fecha_termino').datepicker({
+	format: 'dd-mm-yyyy',
+	onRender: function (date) {
+		return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+	}
+}).on('changeDate', function (ev) {
+	checkout.hide();
+}).data('datepicker');
+$("#total").html(0)
+total=parseFloat($("#total").html())
+i = 0;
+$("#agregar").click(function () {
+	var flag=true
+	// $(this).attr("disabled", flag)
+	$("#detail-items")
+		.append(
+			`<div class="detail-item" id="item-${i}">
+			<span class="pull-right">
+				<button class="btn btn-danger btn-sm" onClick="removeItem(${i})">
+					<i class="fa fa-close"></i>
+				</button>
+			</span>
+			<h5>${$(`#panel1 #headingOne a`).html()}</h5>
+			<p>${$(`#ocupacion-1 option:selected`).text()}</p>
+			<p>niño(s): 0-1 años: 0</p>
+			<p>Estancia corta, Sin Comida</p>
+			<p><span>Total:$ ${$("#panel1 .precio").html()}</span></p>
+		</div>`
+		)
+	i++
+	total += parseFloat($("#panel1 .precio").html())
+	console.log(total)
+	$("#total").html(total)
+});
+$("#agregar2").click(function () {
+	// $(this).attr("disabled", true)
+	$("#detail-items")
+		.append(
+			`<div class="detail-item" id="item-${i}">
+			<span class="pull-right">
+				<button class="btn btn-danger btn-sm" onClick="removeItem(${i})">
+					<i class="fa fa-close"></i>
+				</button>
+			</span>
+			<h5>${$(`#panel2 #headingOne a`).html()}</h5>
+			<p>${$(`#ocupacion-2 option:selected`).text()}</p>
+			<p>niño(s): 0-1 años: 0</p>
+			<p>Estancia corta, Sin Comida</p>
+			<p><span>Total:$ ${$("#panel2 .precio").html()}</span></p>
+		</div>`
+		)
+	i++
+	total += parseFloat($("#panel2 .precio").html())
+	console.log(total)
+	$("#total").html(total)
+});
+function removeItem(i) {
+	$(`#item-${i}`).remove();
+	var flag= false
+	// $("#agregar").attr("disabled", flag)
+	// $("#agregar").attr("disabled", flag)
+}
+console.log(parseFloat($("#panel1 .precio").html()))
+console.log(parseFloat($("#total").html()))
