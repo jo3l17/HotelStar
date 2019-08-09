faeecb2454b9c34d7a80f58c78d8573d9445654c
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2019-08-09 16:24:40 -0500

    motor de reserfva parte 1 terminado

--- public/js/customteam.js
+++ public/js/customteam.js
@@ -210,12 +210,15 @@ var checkout = $('#fecha_termino').datepicker({
 }).on('changeDate', function (ev) {
 	checkout.hide();
 }).data('datepicker');
+var precios = []
 $("#total").html(0)
-total=parseFloat($("#total").html())
+$("#precio-total").html(0)
+total = parseFloat($("#total").html())
 i = 0;
-$("#agregar").click(function () {
-	var flag=true
-	// $(this).attr("disabled", flag)
+$(".agregarItem").click(function () {
+	$(this).addClass("disabled")
+	var itemId = $(this).parent().parent().parent().parent().parent().parent().parent().parent().parent().attr("id")
+	$(this).addClass(`btn-${i}`);
 	$("#detail-items")
 		.append(
 			`<div class="detail-item" id="item-${i}">
@@ -224,45 +227,85 @@ $("#agregar").click(function () {
 					<i class="fa fa-close"></i>
 				</button>
 			</span>
-			<h5>${$(`#panel1 #headingOne a`).html()}</h5>
+			<h5>${$(`#${itemId} .panel-heading a`).html()}</h5>
 			<p>${$(`#ocupacion-1 option:selected`).text()}</p>
 			<p>niño(s): 0-1 años: 0</p>
 			<p>Estancia corta, Sin Comida</p>
-			<p><span>Total:$ ${$("#panel1 .precio").html()}</span></p>
+			<p><span>Total:$ ${$(`#${itemId} .precio`).html()}</span></p>
 		</div>`
 		)
+	precios[i] = parseFloat($(`#${itemId} .precio`).html())
 	i++
-	total += parseFloat($("#panel1 .precio").html())
-	console.log(total)
+	total += parseFloat($(`#${itemId} .precio`).html())
+	// console.log(total)
 	$("#total").html(total)
+	$("#precio-total").html(total)
+	$("#btn-siguiente").removeClass("disabled")
 });
-$("#agregar2").click(function () {
-	// $(this).attr("disabled", true)
-	$("#detail-items")
-		.append(
-			`<div class="detail-item" id="item-${i}">
-			<span class="pull-right">
-				<button class="btn btn-danger btn-sm" onClick="removeItem(${i})">
-					<i class="fa fa-close"></i>
-				</button>
-			</span>
-			<h5>${$(`#panel2 #headingOne a`).html()}</h5>
-			<p>${$(`#ocupacion-2 option:selected`).text()}</p>
-			<p>niño(s): 0-1 años: 0</p>
-			<p>Estancia corta, Sin Comida</p>
-			<p><span>Total:$ ${$("#panel2 .precio").html()}</span></p>
-		</div>`
-		)
-	i++
-	total += parseFloat($("#panel2 .precio").html())
-	console.log(total)
-	$("#total").html(total)
-});
+
 function removeItem(i) {
+	total = total - precios[i]
+	$("#total").html(total)
+	$("#precio-total").html(total)
 	$(`#item-${i}`).remove();
-	var flag= false
-	// $("#agregar").attr("disabled", flag)
-	// $("#agregar").attr("disabled", flag)
+	$(`.btn-${i}`).removeClass("disabled")
+	i--
+	if (total==0){
+		$("#btn-siguiente").addClass("disabled")
+	}
 }
-console.log(parseFloat($("#panel1 .precio").html()))
-console.log(parseFloat($("#total").html()))
+var fixed = $("#detalles-compra")
+$.fn.scrollBottom = function () {
+	return $(document).height() - this.scrollTop() - this.height();
+};
+console.log(fixed)
+function fixDiv() {
+	var $cache = $('#detalles-compra');
+	if ($(window).scrollTop() > 550 && $(window).scrollBottom() > 440) {
+
+		if ($(window).width() > 992) {
+			// $cache.css({
+			// 	'position': 'fixed',
+			// 	'top': '100px'
+			// });
+			$cache.addClass("detalles-compra-fixed")
+		} else { }
+	}
+	else {
+		// $cache.css({
+		// 	'position': 'relative',
+		// 	'top': 'auto'
+		// });
+		$cache.removeClass("detalles-compra-fixed")
+	}
+
+}
+$(window).resize(function () {
+	if ($(window).width() < 992) {
+		fixed.removeClass("detalles-compra-activo")
+		fixed.addClass("detalles-compra-inactivo")
+	} else {
+		fixed.addClass("detalles-compra-activo")
+		fixed.removeClass("detalles-compra-inactivo")
+	}
+});
+
+// console.log($(window).width())
+$(window).scroll(fixDiv);
+fixDiv();
+// console.log(parseFloat($("#panel1 .precio").html()))
+// console.log(parseFloat($("#total").html()))
+$("#detalles-compra-bottom a").click(
+	function mostrardetalles() {
+		fixed.parent().removeClass("visible-md")
+		fixed.parent().removeClass("visible-lg")
+		fixed.removeClass("detalles-compra-inactivo")
+		fixed.addClass("detalles-compra-activo")
+	}
+)
+function cerrarDetalles() {
+	fixed.parent().addClass("visible-md")
+	fixed.parent().addClass("visible-lg")
+	fixed.addClass("detalles-compra-inactivo")
+	fixed.removeClass("detalles-compra-activo")
+}
